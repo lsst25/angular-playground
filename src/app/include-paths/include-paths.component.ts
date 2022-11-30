6e291263d910dc35382ccf8071fb2fe04a0583d8
lsst25 <lsst25@gmail.com>
2022-11-30 22:01:36 +0200

    feat: add a description contetnt to the include-paths page

--- src/app/include-paths/include-paths.component.ts
+++ src/app/include-paths/include-paths.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-include-paths',
   templateUrl: './include-paths.component.html',
-  styleUrls: ['./include-paths.component.css']
+  styleUrls: ['./include-paths.component.scss']
 })
 export class IncludePathsComponent implements OnInit {
 
