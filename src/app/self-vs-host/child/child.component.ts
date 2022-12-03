d018c3479615b7fbf6269729e68b7d5fc6ede8ec
lsst25 <lsst25@gmail.com>
2022-12-03 14:25:41 +0200

    feat(self-va-host): improve content and styles

--- src/app/self-vs-host/child/child.component.ts
+++ src/app/self-vs-host/child/child.component.ts
@@ -9,11 +9,15 @@ export class ChildComponent implements OnInit{
   constructor(@Host() @Optional() private dep: DependencyService) {
   }
 
+  gotDependency: string = '';
+
   ngOnInit() {
     if (this.dep) {
       console.log('dep in Child component is: ',this.dep.exists)
+      this.gotDependency = 'Got dependency'
       return;
     }
+    this.gotDependency = 'Did not get dependency'
     console.log('dep in Child component is: ', this.dep)
   }
 
