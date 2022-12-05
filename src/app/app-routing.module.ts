6aadf3144cdf5797e1586a8b5974eb02b10e5428
lsst25 <lsst25@gmail.com>
2022-12-05 16:34:42 +0200

    feat: add dynamic form route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SelfVsHostComponent } from "./self-vs-host/self-vs-host.component";
 import { IncludePathsComponent } from "./include-paths/include-paths.component";
+import { DynamicFormExampleComponent } from "./dynamic-form-example/dynamic-form-example.component";
 
 const routes: Routes = [
   { path: 'self-vs-host', component: SelfVsHostComponent },
   { path: 'include-paths', component: IncludePathsComponent },
+  { path: 'dynamic-form', component: DynamicFormExampleComponent },
   { path: '**', redirectTo: 'self-vs-host' },
 ];
 
