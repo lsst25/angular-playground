a2d7f35859f7e7054ab19106b3b6302411a28f35
lsst25 <lsst25@gmail.com>
2022-12-09 22:30:04 +0200

    chore: improve header code structure

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,12 @@ import {AppRoutes} from "../app-routing.module";
 })
 export class HeaderComponent {
   readonly appRoutes = AppRoutes;
+
+  readonly navigation = [
+    { route: this.appRoutes.SELF_VS_HOST, title: 'Self vs Host' },
+    { route: this.appRoutes.INCLUDE_PATHS, title: 'An includePaths use' },
+    { route: this.appRoutes.DYNAMIC_FORM, title: 'Dynamic form example' },
+    { route: this.appRoutes.TREE_SHAKING, title: 'Tree shaking example' },
+    { route: this.appRoutes.DOM_MANIPULATIONS, title: 'DOM manipulations' },
+  ]
 }
